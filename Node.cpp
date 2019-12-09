d998437e2f4ff48b56a754a148d01f7bcb48f79b
samzola <353754@bsd48.org>
2019-12-09 07:51:02 -0800

    finished with comments

--- Node.cpp
+++ Node.cpp
@@ -1,32 +1,33 @@
+//imports
 #include <iostream>
 #include "Node.h"
 #include "Student.h"
 
 using namespace std;
 
-Node::Node() {
-  student = NULL;
-  next = NULL;
+Node::Node(Student*) { //constructor, creates node with student
+  student = NULL; //set student to null
+  next = NULL; //set next node to null
 }
 
-Node::~Node() {
-  delete &student;
-  next = NULL;
+Node::~Node() { //destructor
+  delete &student; //deletes student
+  next = NULL; ///sets next node to null
 }
 
-void Node::setStudent(Student* newstudent) {
-  student = newstudent;
+void Node::setStudent(Student* newstudent) { //set student
+  student = newstudent; //sets the student to the input
 }
 
-Student* Node::getStudent() {
+Student* Node::getStudent() { //returns student
   return student;
 }
 
-void Node::setNext(Node* newnext) {
-  next = newnext;
+void Node::setNext(Node* newnext) { //gets next node
+  next = newnext; //set next node to inputted node
 }
 
-Node* Node::getNext() {
+Node* Node::getNext() { //returns next node
   return next;
 }
 
